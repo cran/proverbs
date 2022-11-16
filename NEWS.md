280df5e0d1123a6e38b7c15c2cc74d49dc77dd7c
Brad Lindblad <me@bradlindblad.com>
2022-11-16 10:20:02 +0000

    version 0.2.0

---
+++ NEWS.md
@@ -0,0 +1,9 @@
+# proverbs 0.2.0
+
+## New features
+Added two new arguments to `proverbs::proverb()`
+
+* main_color
+* accent_color
+
+These allow you to specify the actual colors printed to the terminal. They use the colors from the [crayon package](https://github.com/r-lib/crayon#readme).
---
+++ inst/doc/auto_proverb.R
@@ -0,0 +1,27 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ---- eval = FALSE------------------------------------------------------------
+#  library(proverbs)
+#  library(usethis)
+
+## ---- eval = FALSE------------------------------------------------------------
+#  usethis::edit_r_profile(scope = "user")
+
+## ---- eval = FALSE------------------------------------------------------------
+#  if(interactive()) {
+#  
+#  proverbs::proverb()
+#  
+#  }
+
+## ---- eval = FALSE------------------------------------------------------------
+#  if(interactive()) {
+#  
+#  proverbs::proverb(main_color = "black", accent_color = "silver")
+#  
+#  }
+
